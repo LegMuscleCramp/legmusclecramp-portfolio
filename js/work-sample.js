c813c57d797ad3cc9cb560a6c8a524ba6d9641a7
LegMuscleCramp <tito.925.toledo@gmail.com>
2026-04-15 02:04:36 -0400

    new layout for work sample created; work samples arrow cycling in progress

--- js/work-sample.js
+++ js/work-sample.js
@@ -1,18 +1,18 @@
 const workSampleSlider = {
     samples: [],
+    currentSample: 0,
 
     init: function(workSample) {
         this.samples = workSample.querySelectorAll('.work-sample-demos > .work-sample-demo-item');
         
-        const sampleBtns = workSample.querySelectorAll('[class^=demo-] > button')
-        .forEach(sampleBtn => this.clickListener(sampleBtn));
+        workSample.querySelectorAll('[class^=demo-] > button')
+        .forEach(sampleBtn => this.clickListener(sampleBtn, this.samples, this.currentSample));
     },
 
-    clickListener: function(target) {
+    clickListener: function(target, samples, currentSample) {
         target.addEventListener('click',function(e) {
-            const clickTarget = e.currentTarget;
-            
-        })
+            console.log(e)
+        });
     }
 }
 
